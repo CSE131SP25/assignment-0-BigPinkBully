703c330535f038eb427d210c6a75d743d4c215bd
Shawn <Shawn@172.16.72.142>
2025-01-20 12:16:19 -0600

    first commit

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,9 +30,13 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
+		robot.forward(60);
+		/*testing */
 		robot.turnLeft(90);
 		robot.forward(23);
 		robot.turnRight(90);
+		robot.forward(80);
+		robot.turnRight(120);
 		robot.forward(80);
 	}
 
